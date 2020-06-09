0e3b6f237c150af806edcca49078fda186f23bb4
Mathews Motta <mathewscsmotta@gmail.com>
2020-06-09 22:46:37 +0100

    Adicionado projeto

---
+++ Assets/BalanceController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalanceController : MonoBehaviour
+{
+    private Gyroscope _gyroscope;
+    void Start()
+    {
+        _gyroscope = Input.gyro;
+        _gyroscope.enabled = true;
+    }
+
+    void Update()
+    {
+        // attitude
+        var attX = _gyroscope.attitude.x * (-90);
+        var attY = _gyroscope.attitude.y * (-90);
+        
+        transform.rotation = Quaternion.Euler(attX, 0, attY);
+    }
+}
